1e00d6812f4d2755148fa198c6316d03f540b6aa
Amir Masri <masriamir@gmail.com>
2015-04-17 13:34:50 -0400

    Add command line arguments

--- huffman.c
+++ huffman.c
@@ -5,8 +5,13 @@
 #include "node.h"
 #include "tree.h"
 
-int main ( void ) {
-    char *str = "abracadabra!";
+int main ( int argc, char *argv[] ) {
+    if ( argc != 2 ) {
+        fprintf( stderr, "Usage: out text\n" );
+        return 0;
+    }
+
+    char *str = argv[1];
     cf_pair *charmap = build_charmap( str );
     print_map( charmap );
     node *root = build_tree( charmap, 6 );
