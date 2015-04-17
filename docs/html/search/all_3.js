e90965d730196fc96ab515de7552139a69b175a7
Amir Masri <masriamir@gmail.com>
2015-04-17 10:07:41 -0400

    Update tree documentation

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['free_5fnode',['free_node',['../node_8h.html#a7b57940e26d1048f921901492443f67c',1,'node.c']]],
   ['free_5fqueue',['free_queue',['../queue_8h.html#a75dca173dc6068abc37c74b9f6b1b10c',1,'queue.c']]],
+  ['free_5ftree',['free_tree',['../tree_8h.html#ae3252b938b1a968a5b8bba2b72a34807',1,'tree.c']]],
   ['freq',['freq',['../structnode__t.html#a9e42b84da748936e09ca0bb534f21cbc',1,'node_t']]]
 ];
